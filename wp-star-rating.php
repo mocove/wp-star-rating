573b8aea75bb724f783edcaaf649ec5f9d8a89c2
mocove <mocove@gmail.com>
2018-03-26 09:32:44 -0400

    Hello World stage.

--- wp-star-rating.php
+++ wp-star-rating.php
@@ -1,5 +1,24 @@
 <?php
 /*
 Plugin Name: wp-star-rating
+Plugin URI: https://martinove.dk
+Description: WordPress star rating plugin.
+Version 0.0.1
+Author: Martin Ove Christensen
+Author URI: https://martinove.dk
 */
+
+function wpsr_hello_world() {
+  echo "Hello World.";
+}
+
+/*
+Life-cycle hooks
+*/
+
+function wpsr_install() {
+  //activation code
+}
+
+register_activation_hook(__FILE__, 'wpsr_install');
 ?>
