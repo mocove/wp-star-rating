61708c6ab1ae313108ac5e6de7964381cbaa4e98
mocove <mocove@gmail.com>
2018-03-30 21:50:29 +0200

    Lots of stuff.
    Mainly reverting to filters only.

---
+++ main.js
@@ -0,0 +1,4 @@
+function wpsr_rateClick(rating) {
+  //code
+}
+  
--- wp-star-rating.php
+++ wp-star-rating.php
@@ -18,11 +18,31 @@ function wpsr_comment_ratings($comment_id) {
 
 function wpsr_render_ratings($comment_text, $comment) {
     $rating = get_comment_meta($comment->comment_ID, 'wpsr_rate', true);
-    return $rating;
+
+    //check for rating on comment
+    if (isset($rating) && $rating !== '' ) {
+      return 'Rating: ' . $rating . '<br / />' . $comment_text;
+    }
+    else {
+      return $comment_text;
+    }
+}
+
+// function wpsr_recipe_comments_template($comment_template) {
+//   global $post;
+//   return dirname(__FILE__) . '/recipe_comments.php';
+// }
+
+function wpsr_ratings_in_form($fields) {
+  $fields['wpsr_rating'] = '';
+  return $fields;
 }
 
 add_action('comment_post', 'wpsr_comment_ratings');
 add_filter('comment_text', 'wpsr_render_ratings', 10, 3);
+//add_filter('comments_template', 'wpsr_recipe_comments_template');
+add_filter('comment_form_default_fields', 'wpsr_ratings_in_form'); // displays rating in comment form
+wp_enqueue_script('wpsr-script', plugins_url('main.js', __FILE__), array('jquery'));
 
 /*
 Life-cycle hooks
